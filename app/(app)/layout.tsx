41178440edfc0dcd7f1273d8820469a8f8869553
Nikhil Sheoran <thenikhilsheoran@gmail.com>
2025-03-22 18:52:06 +0530

    fixed layout issue

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import OnboardingModal from "@/app/components/onboarding-modal";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-full">
         <OnboardingModal />
         {children}
     </div>
